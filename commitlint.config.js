3fe530fd725d28bf4c2d96b0d82b3fc3ae8e670e
Massimiliano Ziccardi <mziccard@redhat.com>
2020-06-10 15:36:13 +0200

    badtype: fixed commit lint configuration
    The chore comments were not recognized

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-angular'],
 };
+
+const config = require('@commitlint/config-angular');
+const types = config.rules['type-enum'][2];
+
+config.rules['type-enum'][2] = ['chore'].concat(types);
+
+module.exports = config;
